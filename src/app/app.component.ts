1a1be11584cef4475373f0cae66baec7b5fbda89
Administrator <kenneth.lobaton.58@gmail.com>
2022-06-19 10:16:36 +0800

    Sidenav Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {MatSidenav} from "@angular/material/sidenav";
 
 @Component({
@@ -6,12 +6,25 @@ import {MatSidenav} from "@angular/material/sidenav";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'only-fans';
+  innerwidth: any
   @ViewChild('sidenav') sideNav: MatSidenav
+  @HostListener('window:resize', ['$event'])
+
+  ngOnInit(){
+    this.getWidth(innerWidth)
+  }
 
   hamclicked(){
     this.sideNav.toggle()
+  }
+  getWidth(event: any){
+    this.innerwidth = window.innerWidth;
+    if (this.innerwidth >= 992){
+      this.sideNav.close()
+    }
+    // console.log(this.innerwidth)
   }
 }
 
